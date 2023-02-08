0066fe4c2e36ddc7051ef2349f84366d681a8987
harunws <harunws33@gmail.com>
2023-02-08 22:33:56 +0100

    router is working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-
-
+import {Link, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
-  );
+    <>
+      <nav>
+        <ul>
+          <li> <Link to="/"> Home </Link></li>
+          <li> <Link to="/about"> About </Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About/>}/>
+      </Routes>
+    </>
+  )   
 }
 
 export default App;
